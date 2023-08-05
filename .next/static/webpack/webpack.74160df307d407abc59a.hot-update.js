ac26e89718a870edfc1726d0ff340523d7907aed
ValdryanIvandito <valdryan08@gmail.com>
2023-08-05 15:46:49 +0700

    add .next file

---
+++ .next/static/webpack/webpack.74160df307d407abc59a.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "10fcdded820a76f1b9d6"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay43NDE2MGRmMzA3ZDQwN2FiYzU5YS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjEwZmNkZGVkODIwYTc2ZjFiOWQ2XCI7IH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
