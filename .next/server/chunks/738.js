aee9f71eb6e609051c7098931b60febce9acb5bb
ValdryanIvandito <valdryan08@gmail.com>
2023-08-05 15:52:23 +0700

    builded project

---
+++ .next/server/chunks/738.js
@@ -0,0 +1,66 @@
+exports.id = 738;
+exports.ids = [738];
+exports.modules = {
+
+/***/ 3738:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Features)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_feature_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2367);
+/* harmony import */ var _styles_feature_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_feature_module_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+function Features() {
+  const items = [{
+    title: "Rock Paper Scissor",
+    description: "Game Bantung Gunting Kertas melawan computer",
+    backgroundImage: '/images/rock-paper-scissor.jpg'
+  }, {
+    title: "Head or Tails",
+    description: "Mencoba menebak hasil dari lemparan coin",
+    backgroundImage: '/images/coin.jpg'
+  }, {
+    title: "Game Dadu",
+    description: "Permainan lempar dadu melawan komputer",
+    backgroundImage: '/images/dadu.jpg'
+  }];
+  const fontSize = {
+    fontSize: '16px'
+  };
+  return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("section", {
+    children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+      className: (_styles_feature_module_css__WEBPACK_IMPORTED_MODULE_2___default().FeaturePageImage)
+    })
+  });
+}
+
+/***/ }),
+
+/***/ 2367:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"FeaturePageImage": "feature_FeaturePageImage__15vSC",
+	"featureTitle": "feature_featureTitle__3j_jX",
+	"featureDescription": "feature_featureDescription__1_igv",
+	"mderwSm": "feature_mderwSm__1hnjs",
+	"rectangleItem": "feature_rectangleItem__RYjPa",
+	"rectangleItemBefore": "feature_rectangleItemBefore__9m5vj",
+	"rectangleItemContent": "feature_rectangleItemContent__1VXOY"
+};
+
+
+/***/ })
+
+};
+;
